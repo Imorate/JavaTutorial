2e2a34907b4b531a057fe62ceb5355a0898c5395
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-01-06 00:43:02 +0330

    Add Point class

---
+++ src/main/java/core/oop/Point.java
@@ -0,0 +1,9 @@
+package core.oop;
+
+import lombok.Data;
+
+@Data
+public class Point {
+    private int x;
+    private int y;
+}
---
+++ src/test/java/core/oop/PointTest.java
@@ -0,0 +1,25 @@
+package core.oop;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("Point class test")
+class PointTest {
+    @Test
+    @DisplayName("Second point object should be referenced to the first point object")
+    void secondPointShouldBeReferencedToFirstPoint() {
+        Point p1 = new Point();
+        p1.setX(10);
+        p1.setY(20);
+
+        @SuppressWarnings("UnnecessaryLocalVariable")
+        // Set p2 reference to the p1
+        Point p2 = p1;
+        p2.setX(0);
+
+        assertEquals(0, p1.getX());
+        assertEquals(20, p1.getY());
+    }
+}

f4eb446478c6d6323b4ecab2669b693623f81777
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-05-10 01:07:06 +0330

    Add ThreadLogger for better debugging

---
+++ src/main/java/javacup/multithreading/common/ThreadLogger.java
@@ -0,0 +1,24 @@
+package javacup.multithreading.common;
+
+public class ThreadLogger {
+    public static void log(String template, Object... objects) {
+        log(Thread.currentThread(), template, objects);
+    }
+
+    public static void log(Thread thread, String template) {
+        log(thread, template, (Object) null);
+    }
+
+    public static void log(String template) {
+        log(Thread.currentThread(), template, (Object) null);
+    }
+
+    public static void log(Thread thread, String template, Object... objects) {
+        String threadDetails = String.format("%s-%s:\t", thread.getId(), thread.getName());
+        if (objects == null) {
+            System.out.printf(threadDetails + template + "\n");
+        } else {
+            System.out.printf(threadDetails + template + "\n", objects);
+        }
+    }
+}
